915fc74f51c9b8b72dc00fee525946c778a49276
William R Poulson <wrpoulson@gmail.com>
2017-09-16 13:11:31 -0400

    Fun with Functions done

--- PythonOneHour/exercise_1.py
+++ PythonOneHour/exercise_1.py
@@ -1,21 +1,34 @@
-import collections
+import collections55
 
-try:
-    fh = open("C:/Git/Python/PythonOneHour/lesson_1_input.txt", "r", encoding="ascii")
-except OSError as e:
-    print("error %d reading file %s" % (e.errno, e.filename))
-    quit()
+def openFile():
+    fh = None
+    try:
+        fh = open("C:/Git/Python/PythonOneHour/lesson_1_input.txt", "r", encoding="ascii")
+    except OSError as e:
+        print("error %d reading file %s" % (e.errno, e.filename))
+        quit()
+    return fh
 
-numWords = 0
-wordFreq = collections.Counter()
-for line in fh:
-    words = line.split()
-    numWords += len(words)
-    for word in words:
-        wordFreq[word] += 1
-fh.close()
+def processFile(fh):
+    numWords = 0
+    wordFreq = collections.Counter()
+    for line in fh:
+        words = line.split()
+        numWords += len(words)
+        for word in words:
+            wordFreq[word] += 1
+    fh.close()
+    return numWords, wordFreq
 
+def printReport(numWords, wordFreq):
+    print("\nThere are a total of {0} words".format(numWords))
+    for key in sorted(wordFreq.keys()):
+        print("key %-12s found %2d times" % (key, wordFreq[key]))
 
-print("\nThere are a total of {0} words".format(numWords))
-for key in sorted(wordFreq.keys()):
-    print("key %-12s found %2d times" % (key, wordFreq[key]))
+def main():
+    fh = openFile()
+    (nw, wf) = processFile(fh)
+    printReport(nw, wf)
+
+if __name__ == "__main__":
+    main()
