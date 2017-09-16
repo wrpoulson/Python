e64c2ac7fff65c70cd33e3ea841304b511a2c679
William R Poulson <wrpoulson@gmail.com>
2017-09-16 13:24:13 -0400

    dictionary, list, set complete

--- PythonOneHour/exercise_1.py
+++ PythonOneHour/exercise_1.py
@@ -1,4 +1,4 @@
-import collections55
+import collections
 
 def openFile():
     fh = None
@@ -12,23 +12,30 @@ def openFile():
 def processFile(fh):
     numWords = 0
     wordFreq = collections.Counter()
+    wordIndex = collections.defaultdict(set)
+    lineNum = 0
     for line in fh:
+        lineNum += 1
         words = line.split()
         numWords += len(words)
         for word in words:
             wordFreq[word] += 1
+            wordIndex[word].add(str(lineNum))
     fh.close()
-    return numWords, wordFreq
+    return (numWords, wordFreq, wordIndex)
 
-def printReport(numWords, wordFreq):
+def printReport(stats):
+    (numWords, wordFreq, wordIndex)= stats
     print("\nThere are a total of {0} words".format(numWords))
     for key in sorted(wordFreq.keys()):
         print("key %-12s found %2d times" % (key, wordFreq[key]))
+    for key in sorted(wordIndex.keys()):
+        print("key %-12s found on lines: %s" % (key, ",".join(wordIndex[key])))
 
 def main():
     fh = openFile()
-    (nw, wf) = processFile(fh)
-    printReport(nw, wf)
+    stats = processFile(fh)
+    printReport(stats)
 
 if __name__ == "__main__":
     main()
